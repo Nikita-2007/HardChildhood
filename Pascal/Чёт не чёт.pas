3dc1567584b1238357378f29fde27002c03712e6
Nikita-2007 <Nikita2007ua@mail.ru>
2019-10-20 21:03:37 +0300

    Create Чёт не чёт.pas

---
+++ Pascal/Чёт не чёт.pas
@@ -0,0 +1,23 @@
+﻿Program Чёт_не_чёт;
+
+var X,Y:integer;
+label m;
+
+begin
+  
+  m:
+  writeln ('Введите число');
+  read (X);
+  Y := X mod 2;
+  if Y = 0 then
+    begin
+    writeln ('Ваша число чёткое');
+    goto m;
+    end
+    else
+      begin
+      writeln ('Ваша число не чёткое');
+  goto m;
+  end
+  
+end.
