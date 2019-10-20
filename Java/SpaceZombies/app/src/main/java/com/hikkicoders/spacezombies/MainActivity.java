57ca1188e2cd2172a5bdf99728373623352b1f92
Nikita-2007 <Nikita2007ua@mail.ru>
2019-10-20 21:10:18 +0300

    Create MainActivity.java

---
+++ Java/SpaceZombies/app/src/main/java/com/hikkicoders/spacezombies/MainActivity.java
@@ -0,0 +1,29 @@
+package com.hikkicoders.spacezombies;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnStart(View view) {
+        Intent intent = new Intent (MainActivity.this, lvl1.class);
+        startActivity(intent);
+    }
+
+    public void btnCont(View view) {
+    }
+
+    public void btnEx(View view) {
+        finish();
+        System.exit(0);
+    }
+
+}
