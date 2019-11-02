ba40bff85e07d91dbaea4412359bc85b7ca9d3bb
Nikita-2007 <nikita2007ua@gmail.com>
2019-11-02 21:21:58 +0300

    Update Tetris.java

--- Java/Tetris/Tetris.java
+++ Java/Tetris/Tetris.java
@@ -4,6 +4,7 @@
  * Октябрь
  * 2019
  */
+ 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyAdapter;
@@ -22,6 +23,7 @@ public class Tetris extends JPanel {
 	 *  🡻
 	 */
 	protected static int block = 40, x = 3, y = -3, randForm;
+	public int ground[][][] = new int [20][10][3];
 	public int form[][][] = {
 		{{0, 2}, {1, 2}, {2, 2}, {3, 2}, {  0, 255, 255}}, //I
 		{{1, 2}, {2, 2}, {2, 1}, {2, 0}, {  0,   0, 255}}, //J
@@ -29,7 +31,7 @@ public class Tetris extends JPanel {
 		{{1, 2}, {2, 2}, {1, 1}, {2, 1}, {255, 255,   0}}, //O
 		{{0, 2}, {1, 2}, {1, 1}, {2, 1}, {  0, 255,   0}}, //S
 		{{1, 2}, {0, 1}, {1, 1}, {2, 1}, {255,   0, 255}}, //T
-		{{1, 2}, {2, 2}, {0, 1}, {1, 1}, {255,   0,   0}}  //Z
+		{{1, 2}, {2, 2}, {0, 1}, {1, 1}, {255,   0,   0}}  //Z 
 	};
 	Random random = new Random();
 	
@@ -98,13 +100,29 @@ public class Tetris extends JPanel {
 
 	public void paint(Graphics ctx) {
 		super.paint(ctx);
-		setBackground(Color.black);
+		
+		//Дно
+		for(int i = 0; i < 20; i++){
+			for(int j = 0; j < 10; j++) {
+				ctx.setColor(new Color(ground[i][j][0], ground[i][j][1], ground[i][j][2]));
+				ctx.fillRect(j*block, i*block, block, block);
+			}
+		}
+		// фигуры
 		for(int i = 0; i < 4; i++) {
 			ctx.setColor(new Color(form[randForm][4][0], form[randForm][4][1], form[randForm][4][2]));
 			ctx.fillRect(block*form[randForm][i][0]+x*block, block*form[randForm][i][1]+y*block, block, block);
 		}
+		
+		//сетка
 		ctx.setColor(Color.gray);
 		for(int i = 0; i < 10; i++) ctx.drawLine(block*i,0 ,block*i , block*20);
 		for(int i = 0; i < 20; i++) ctx.drawLine(0, block*i, block*10, block*i);
 	}
 }
+	/*Твоя самооценка
+	 *  |
+	 *  |
+	 *  🡻
+	 *  .
+	 */ 
