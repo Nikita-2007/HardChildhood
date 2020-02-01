5c47d95b3c9a9cf075b3942d374ba4f75ebd197f
Nikita-2007 <nikita2007ua@gmail.com>
2020-02-01 21:04:10 +0300

    Create HelloWorld.cs

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,21 @@
+/*
+ * Уроки по C#
+ * Никкита
+ * 01.02.2020
+ * HelloWorld 
+ */
+
+using System;
+
+namespace MyProgram
+{
+	class HW
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+			Console.WriteLine(DateTime.Now);
+			Console.ReadKey();
+		}
+	}
+}
