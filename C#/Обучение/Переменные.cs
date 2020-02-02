0fa30f8795887ec5aa7683218f133c949a25cfbb
Nikita-2007 <nikita2007ua@gmail.com>
2020-02-02 19:13:48 +0300

    Create Переменные.cs

---
+++ C#/Обучение/Переменные.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Обучение
+{
+    class Переменные
+    { 
+            public void Консоль()
+        {
+            Console.Write("Сравнение 2>3 - ");
+            Console.WriteLine(2>3);
+            Console.WriteLine();
+
+            bool x = 2 > 3;
+            Console.Write("Сравнение 2>3 - ");
+            Console.WriteLine(x);
+            Console.WriteLine();
+
+            bool y = 2 < 3;
+            Console.Write("Сравнение 2<3 - ");
+            Console.WriteLine(y);
+            Console.WriteLine();
+
+            bool z = 2 == 3;
+            Console.Write("Сравнение 2=3 - ");
+            Console.WriteLine(z);
+            Console.WriteLine();
+        }
+    }
+}
