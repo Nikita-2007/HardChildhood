b26b299013084f11c4fdf2499da5641aa9104f4e
Nikita-2007 <nikita2007ua@gmail.com>
2020-02-08 16:38:49 +0300

    Create Условия.cs

---
+++ C#/Обучение/Условия.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Обучения2
+{
+    class Условия
+    {
+        public void Консоль2()
+        {
+            bool x = 2 < 3;
+            if (x)
+            {
+                Console.WriteLine($"2 Больше 3 {0}\n");
+            }
+            else
+            {
+                Console.WriteLine($"2 Не Больше 3 {0}\n");
+            }
+        }
+    }
+}
