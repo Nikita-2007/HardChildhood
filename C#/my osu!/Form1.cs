66ffbce3beae096ab792f52a3da6c545b436523f
Nikita-2007 <nikita2007ua@gmail.com>
2020-02-22 21:36:59 +0300

    Update Form1.cs

--- C#/my osu!/Form1.cs
+++ C#/my osu!/Form1.cs
@@ -16,17 +16,18 @@ namespace my_osu_
         //Переменые и объекты
         private Random random = new Random();
         private Bitmap target = Resource1.cursor;
-        private Point point = Point.Empty; 
-        //private Bitmap Krug = Resource1.Krug;
+        private Point point = Point.Empty;
+        private int score;
 
-        //Зпуск окна
+        //Запуск окна
         public Form1()
         {
             InitializeComponent();
-            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | 
                      ControlStyles.AllPaintingInWmPaint |
                      ControlStyles.UserPaint, true);
             UpdateStyles();
+            randomTarget();
         }
 
         //Отрисовка окна
@@ -34,29 +35,41 @@ namespace my_osu_
         {
             Graphics ctx = e.Graphics;
             ctx.SmoothingMode = SmoothingMode.AntiAlias;
-            int score = 0;
-            score++;
+            Cursor.Hide();
             label1.Text = score.ToString();
 
-            var position = this.PointToClient(Cursor.Position);
+            Point position = this.PointToClient(Cursor.Position);
 
-            point.X = 600 + random.Next(-4, 4)*100;
-            point.Y = 400 + random.Next(-3, 3)*100;
 
-            //ctx.DrawImage(Krug, 100, 100, 100, 100);
-            ctx.DrawEllipse(new Pen(Color.Red, 2), point.X, point.Y, 100, 100);
-            ctx.DrawImage(target, position.X - 50, position.Y - 50, 100, 100);
+
+            Rectangle krugPosition = new Rectangle(point.X, point.Y, 100, 100);
+            Rectangle cursorPosition = new Rectangle(position.X - 50, position.Y - 50, 100, 100);
+
+            ctx.DrawEllipse(new Pen(Color.Red, 2), krugPosition);
+            ctx.DrawImage(target, cursorPosition); 
         }
 
         //Обновления
         private void timer1_Tick(object sender, EventArgs e)
         {
+            score++;
             Refresh();
         }
-
-        private void label1_Click(object sender, EventArgs e)
+        //Движение Круга
+        private void randomTarget()
         {
-            
+            point.X = 600 + random.Next(-4, 4) * 100;
+            point.Y = 400 + random.Next(-3, 3) * 100;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            randomTarget();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            randomTarget();
         }
     }
 }
