3c128524da3e996ccd60a09cca2f32939f7484cd
Nikita-2007 <nikita2007ua@gmail.com>
2020-03-08 15:18:48 +0300

    Create Program.cs

---
+++ C#/Password/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Переменые
+            string password = "1234", _password = "";
+
+            //Проверка пороля
+            while (password != _password) {
+                Console.Write("Ведите пороль: ");
+                _password = Console.ReadLine();
+                if (_password != password) Console.WriteLine("К доступу нет доступа");
+             }
+
+            //Доступ
+            Console.WriteLine("Доступ разрешили");
+            Console.ReadKey();
+        }
+    }
+}
