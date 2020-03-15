a83d4e4a7fb1e01aa44d76f4a85484cbcf723c84
Nikita-2007 <nikita2007ua@gmail.com>
2020-03-15 16:49:14 +0300

    Update Переменные.cs

--- C#/Обучение/Переменные.cs
+++ C#/Обучение/Переменные.cs
@@ -4,7 +4,7 @@ namespace Обучение
 {
     class Переменные
     { 
-            public void Консоль()
+        public void Консоль()
         {
             Console.Write("Сравнение 2>3 - ");
             Console.WriteLine(2>3);
@@ -18,6 +18,26 @@ namespace Обучение
 
             bool z = 2 == 3;
             Console.WriteLine($"Сравнение 2=3 - {z}\n");
+
+            int a = 16;
+            Console.WriteLine($"Остаток от деления 16/5 - {a%5}\n");
+
+            Console.WriteLine($"Побитовый сдвиг вниз 16 >> 1 - {a >>= 1}\n");
+
+            Console.WriteLine($"Побитовый сдвиг вниз 16 << 1 - {a <<= 2}\n");
+
+            int b = 100, c = 3;
+            decimal d = (decimal) b / c;
+            double e = (double)b / c;
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+
+            Console.WriteLine($"Бинарный код 0b100001 - {0b100001}\n");
+            Console.WriteLine($"Байт код 0xA1 - {0xA1}\n");
+            Console.WriteLine($"Чисто в степени  3.2e4- {3.2e4}\n");
+            Console.WriteLine($"Чисто в степени  1.2e-5 - {1.2e-3}\n");
+            Console.WriteLine($"Юни-код u0420 - {"\u0420"}\n");
+            Console.WriteLine($"ASKII - {"\x5A"}\n");
         }
     }
 }
