065e9ee2ee5371a3894038c0e6395777cf5a98f9
Nikita-2007 <nikita2007ua@gmail.com>
2020-04-01 21:21:19 +0300

    Create ListTank.cs

---
+++ C#/Tanks/ListTank.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace Tanks
+{
+    class ListTank
+    {
+        Tank tank;
+
+        public void DrawListTank(Graphics g)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                tank = new Tank();
+                tank.DrawTank(g);
+            }
+        }
+    }
+}
