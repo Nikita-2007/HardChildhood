24a8aedcb6468f0ada180745e3b0b7f262149434
Nikita-2007 <nikita2007ua@gmail.com>
2020-04-02 11:15:43 +0300

    Update ListTank.cs

--- C#/Tanks/ListTank.cs
+++ C#/Tanks/ListTank.cs
@@ -1,16 +1,33 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 
 namespace Tanks
 {
-    class ListTank
+    class ListTank : List<Tank>
     {
-        Tank tank;
+        Tank tank = new Tank();
+        List<Tank> listTanks = new List<Tank>();
 
+
+        //создаём список танков
+        public List<Tank> CreateListTank()
+        {
+            for (byte  i = 1; i <= 10; i++)
+            {
+                listTanks.Add(new Tank()
+                {
+                    id = i,
+                    position = tank.Position()
+                });
+            }
+            return listTanks;
+        }
+
+        // отрисовка списка танков
         public void DrawListTank(Graphics g)
         {
-            for (int i = 0; i < 10; i++)
+            foreach (Tank tank in listTanks)
             {
-                tank = new Tank();
                 tank.DrawTank(g);
             }
         }
