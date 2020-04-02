0fbcc1106317a396b31599484c7423f928eaad9c
Nikita-2007 <nikita2007ua@gmail.com>
2020-04-02 11:15:35 +0300

    Update FormTank.cs

--- C#/Tanks/FormTank.cs
+++ C#/Tanks/FormTank.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; 
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -9,6 +9,7 @@ namespace Tanks
         Graphics g;
         ListTank listTank;
 
+        //Окно приложения
         public FormTank()
         {
             InitializeComponent();
@@ -18,29 +19,31 @@ namespace Tanks
             UpdateStyles();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        //Загрузка окна
+        private void FormTank_Load(object sender, EventArgs e)
         {
-            Refresh();
-
-        }
-        int x = 0;
-        private void FormTank_Click(object sender, EventArgs e)
-        {
-            if (timer.Enabled == false) timer.Enabled = true;
-            else timer.Enabled = false;
+            listTank = new ListTank();
+            listTank.CreateListTank();
         }
 
+        //Обновление окна
         private void FormTank_Paint(object sender, PaintEventArgs e)
         {
-            x++;
-            label1.Text = x.ToString();
             g = e.Graphics;
             listTank.DrawListTank(g);
         }
 
-        private void FormTank_Load(object sender, EventArgs e)
+        //Таймер
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            listTank = new ListTank();
+            Refresh();
+        }
+
+        //Клик
+        private void FormTank_Click(object sender, EventArgs e)
+        {
+            if (timer.Enabled == false) timer.Enabled = true;
+            else timer.Enabled = false;
         }
     }
 }
