164ddaedfc306daad62e63957e26316f482250f1
Nikita-2007 <nikita2007ua@gmail.com>
2020-04-12 12:07:13 +0300

    Update Tank.cs

--- C#/Tanks/Tank.cs
+++ C#/Tanks/Tank.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 
 namespace Tanks
 {
-    class Tank
-    {
-        public int id; // Я тепя по ID вычеслю
-        public PointF position; //Местоположение Танк
-
+    class Tank : Unit
+    {       
         private Bitmap bitmap = new Bitmap(Properties.Resources.Танк);
         private Rectangle body = new Rectangle(new Point(0, 0), new Size(128, 128));
-        private Rectangle tower = new Rectangle(new Point(128, 0), new Size(256, 128));
-        private float vector; // Угол поворота корпуса
+        private Rectangle tower = new Rectangle(new Point(128, 0), new Size(256, 128));       
         private float vectorTower; // Угол поворота башни
-        private float speed = 1; // скорость
-        private Point target; //цель
  
         //Отрисовка Танка
         public void DrawTank(Graphics g, Point cursor)
         {
+            speed = 1;
             target = cursor;
             Position();
             Vector();
@@ -35,24 +29,6 @@ namespace Tanks
             g.RotateTransform(vectorTower);
             g.DrawImage(bitmap, -64, -90, tower, GraphicsUnit.Pixel);
             g.ResetTransform();
-        }
-
-        //Поворот танка
-        private float Vector()
-        {
-            float catetX = target.X - position.X;
-            float catetY = target.Y - position.Y;
-            vector = (float)(Math.Atan2(catetY, catetX) * 180/Math.PI+90);
-
-            return vector;
-        }
-
-        //Позиция танка
-        private PointF Position()
-        {
-            position.X += speed*(float)Math.Cos(vector);
-            position.Y += speed*(float)Math.Sin(vector);
-            return position;
         }
     }
 }
