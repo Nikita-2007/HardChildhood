5b502a070c66090cd962a3fc4b17b73162b30ac9
Nikita-2007 <nikita2007ua@gmail.com>
2020-04-12 12:07:22 +0300

    Update Resources.Designer.cs

--- C#/Tanks/Properties/Resources.Designer.cs
+++ C#/Tanks/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Tanks.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Машинка {
+            get {
+                object obj = ResourceManager.GetObject("Машинка", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Танк {
             get {
                 object obj = ResourceManager.GetObject("Танк", resourceCulture);
