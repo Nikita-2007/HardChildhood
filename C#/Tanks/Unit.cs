7590651c2897d81d7e3e36effe94ffba712f94a2
Nikita-2007 <nikita2007ua@gmail.com>
2020-04-20 15:11:27 +0300

    Update Unit.cs

--- C#/Tanks/Unit.cs
+++ C#/Tanks/Unit.cs
@@ -5,11 +5,29 @@ namespace Tanks
 {
     abstract class Unit
     {
-        public int id; // Я тепя по ID вычеслю
+        private static uint ID; 
+
+        public uint id = ++ID;// Я тепя по ID вычеслю
         public PointF position; //Местоположение Танк
         public float vector; // Угол поворота корпуса
         public float speed = 2; // скорость
         public Point target; //цель
+        private Font font = new Font("Areal", 16, FontStyle.Bold, GraphicsUnit.Point);
+        private SolidBrush color = new SolidBrush(Color.Black);
+        private Pen pen = new Pen(Color.Chartreuse, 5);
+
+        //Номер и полоска жизини
+        public void DrawInfo(Graphics g)
+        {
+            //ID
+            g.TranslateTransform(position.X, position.Y);
+            g.DrawString(id.ToString(), font, color, -10, -50);            
+            g.ResetTransform();
+            //Полоска жизни
+            g.TranslateTransform(position.X, position.Y);
+            g.DrawLine(pen, -30, 30, 40, 30);
+            g.ResetTransform();
+        }
 
         //Позиция
         public PointF Position()
