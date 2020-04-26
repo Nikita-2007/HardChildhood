de31c1536a91c6b563f487d62fdfd1904eb4607f
Nikita-2007 <nikita2007ua@gmail.com>
2020-04-26 12:17:02 +0300

    Update Unit.cs

--- C#/Tanks/Unit.cs
+++ C#/Tanks/Unit.cs
@@ -15,6 +15,7 @@ namespace Tanks
         private Font font = new Font("Areal", 16, FontStyle.Bold, GraphicsUnit.Point);
         private SolidBrush color = new SolidBrush(Color.Black);
         private Pen pen = new Pen(Color.Chartreuse, 5);
+        private float angle;
 
         //Номер и полоска жизини
         public void DrawInfo(Graphics g)
@@ -29,22 +30,39 @@ namespace Tanks
             g.ResetTransform();
         }
 
-        //Позиция
-        public PointF Position()
-        {
-            position.X += speed * (float)Math.Cos(vector);
-            position.Y += speed * (float)Math.Sin(vector);
-            return position;
-        }
-
         //Поворот
         public float Vector()
         {
+            // угол на цель
             float catetX = target.X - position.X;
             float catetY = target.Y - position.Y;
-            vector = (float)(Math.Atan2(catetY, catetX) * 180 / Math.PI + 90);
+            angle = (float)(Math.Atan2(catetY, catetX) * 180 / Math.PI + 90);
+            if (angle < 0) angle += 360;
+
+            //текущий угол
+            if (Math.Abs(vector - angle) > speed) 
+            {
+                if ((vector < angle && (angle - vector) < 180) ^ (angle - vector) > -180)
+                    vector = (vector - speed+360)%360;
+                else
+                    vector = (vector + speed)%360;
+            }
+            else
+                vector = angle;
+            
 
             return vector;
+        }
+
+        //Позиция
+        public PointF Position()
+        {
+            if (vector == angle)
+            {
+                position.X += speed * (float)Math.Cos(vector);
+                position.Y += speed * (float)Math.Sin(vector);
+            }
+            return position;
         }
     }
 }
