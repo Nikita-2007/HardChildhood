ba188982e529f20aeee42c4fdafeff3b995e9786
Nikita-2007 <nikita2007ua@gmail.com>
2020-05-06 21:47:21 +0300

    Create Game.cs

---
+++ C#/Tanks/Game.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Tanks
+{
+    class Game
+    {
+        private ListUnit RedParty, BlueParty;
+        private ListShot listShot;
+        Shot shot;
+
+        //Старт игры
+        public void StartGame()
+        {
+
+            //Красные
+            RedParty = new ListUnit();
+            RedParty.CreateListUnits();
+
+            //Сниние
+            BlueParty = new ListUnit();
+            BlueParty.CreateListUnits();
+
+            //SW();
+            Sound();
+
+            shot = new Shot();
+            shot.position = new PointF(50, 50);
+            shot.target = new PointF(1000, 600);
+            shot.speed = 20;
+        }
+
+        //Шаг игры
+        public void StepGame(Graphics g, Point cursor)
+        {
+            RedParty.DrawListUnit(g, cursor);
+            BlueParty.DrawListUnit(g, cursor);
+            shot.DrawShot(g);
+        }
+
+        //Звук заставки
+        private void SW()
+        {
+            Console.Beep(440, 500);
+            Console.Beep(440, 500);
+            Console.Beep(440, 500);
+            Console.Beep(349, 350);
+            Console.Beep(523, 150);
+            Console.Beep(440, 500);
+            Console.Beep(349, 350);
+            Console.Beep(523, 150);
+            Console.Beep(440, 1000);
+            Console.Beep(659, 500);
+            Console.Beep(659, 500);
+            Console.Beep(659, 500);
+            Console.Beep(698, 350);
+            Console.Beep(523, 150);
+            Console.Beep(415, 500);
+            Console.Beep(349, 350);
+            Console.Beep(523, 150);
+            Console.Beep(440, 1000);
+        }
+        async private void Sound()
+        {
+            await Task.Run(() =>
+            {
+                Console.Beep(440, 500);
+                Console.Beep(500, 500);
+                Console.Beep(440, 500);
+                Console.Beep(300, 500);
+                Console.Beep(37, 1);
+                Console.Beep(275, 200);
+                Console.Beep(250, 200);
+                Console.Beep(225, 200);
+                Console.Beep(200, 200);
+                Console.Beep(150, 750);
+            });
+        }
+    }
+}
