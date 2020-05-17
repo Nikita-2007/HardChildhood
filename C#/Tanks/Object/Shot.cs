bfdcb98e0bd6cccdb9b95d49394de768a6c5214e
Nikita-2007 <nikita2007ua@gmail.com>
2020-05-17 11:21:17 +0300

    Create Shot.cs

---
+++ C#/Tanks/Object/Shot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace Tanks
+{
+    class Shot : AObject
+    {
+        private Pen pen;
+        private PointF position0;
+
+        /// <summary>
+        /// Конструктор снаряда
+        /// </summary>
+        public Shot()
+        {
+            speed = 16;
+        }
+
+        //Отрисовка Пульки
+        public void DrawShot(Graphics g)
+        {
+            position0 = position;
+            position = Position();
+            pen = new Pen(color, 3);
+            g.DrawLine(pen, position, position0);
+        }
+    }
+}
