4c67346a12d52f5d11a3ea4db39a1fdb1a6c3ea9
Nikita-2007 <nikita2007ua@gmail.com>
2020-05-24 11:35:43 +0300

    Create Sound.cs

---
+++ C#/Tanks/Resources/Sound.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tanks
+{
+    class Sound
+    {
+        //Звук заставки
+       public static void SW()
+       {
+           Console.Beep(440, 500);
+           Console.Beep(440, 500);
+           Console.Beep(440, 500);
+           Console.Beep(349, 350);
+           Console.Beep(523, 150);
+           Console.Beep(440, 500);
+           Console.Beep(349, 350);
+           Console.Beep(523, 150);
+           Console.Beep(440, 1000);
+           Console.Beep(659, 500);
+           Console.Beep(659, 500);
+           Console.Beep(659, 500);
+           Console.Beep(698, 350);
+           Console.Beep(523, 150);
+           Console.Beep(415, 500);
+           Console.Beep(349, 350);
+           Console.Beep(523, 150);
+           Console.Beep(440, 1000);
+       }
+        async public static void Music()
+        {
+            await Task.Run(() =>
+            {
+                Console.Beep(440, 500);
+                Console.Beep(500, 500);
+                Console.Beep(440, 500);
+                Console.Beep(300, 500);
+                Console.Beep(37, 1);
+                Console.Beep(275, 200);
+                Console.Beep(250, 200);
+                Console.Beep(225, 200);
+                Console.Beep(200, 200);
+                Console.Beep(150, 750);
+            });
+        }
+    }
+}
