acb0d68fcf031d8327b12c3b312b9ffb4841be37
Nikita-2007 <nikita2007ua@gmail.com>
2020-05-24 11:35:39 +0300

    Update Car.cs

--- C#/Tanks/Object/Car.cs
+++ C#/Tanks/Object/Car.cs
@@ -7,7 +7,7 @@ namespace Tanks
         private static Size size = new Size(64,64);
         private readonly Bitmap bitmap = new Bitmap(Properties.Resources.Машинка);
         private readonly Rectangle body = new Rectangle(new Point(0, 0), size);
-        private Pen pen;
+        private SolidBrush solidBrush;
 
         /// <summary>
         /// Конструктор Машинки
@@ -18,20 +18,24 @@ namespace Tanks
             life = 10;
         }
 
-        //Отрисовка Танка
+        //Отрисовка Машинки
         public void DrawUnit(Graphics g, Point cursor)
         {
+            solidBrush = new SolidBrush(color);
             target = cursor;
             PositionUnit();
             vector = Vector(vector, speed=4);
 
-            //Отрисовка Машинка
+            //Цвет
             g.TranslateTransform(position.X, position.Y);
             g.RotateTransform(vector);
+            g.FillRectangle(solidBrush, -12, -16, 20, 56);
+
+            //Отрисовка Машинка
+
             g.DrawImage(bitmap, -15, -20, body, GraphicsUnit.Pixel);
             g.ResetTransform();
-            pen = new Pen(color, 2);
-            g.DrawEllipse(pen, position.X-10, position.Y-10, 20, 20);
+
 
             DrawInfo(g);           
         }
