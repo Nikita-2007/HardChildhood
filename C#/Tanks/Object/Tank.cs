6a30d58897ab92aa0724c4167e18f9f8b2ffc065
Nikita-2007 <nikita2007ua@gmail.com>
2020-05-27 21:21:44 +0300

    Update Tank.cs

--- C#/Tanks/Object/Tank.cs
+++ C#/Tanks/Object/Tank.cs
@@ -14,8 +14,9 @@ namespace Tanks
         /// <summary>
         /// Конструктор Танка
         /// </summary>
-        public Tank()
+        public Tank(Color color)
         {
+            this.color = color;
             speed = 1;
             life = 40;
         }
