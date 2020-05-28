fc30ff9d6d558851492b73df354d75e8921080e2
Nikita-2007 <nikita2007ua@gmail.com>
2020-05-28 20:50:28 +0300

    Update Game.cs

--- C#/Tanks/Game.cs
+++ C#/Tanks/Game.cs
@@ -13,9 +13,10 @@ namespace Tanks
         {
             ListParty = new List<ListUnit>();
 
-            ListParty.Add(new ListUnit(Color.Red, new Point(30,20)));
-            ListParty.Add(new ListUnit(Color.Blue, new Point (70, 80)));
+            ListParty.Add(new ListUnit(Color.Red, new Point(10,10)));
+            ListParty.Add(new ListUnit(Color.Blue, new Point (80, 80)));
             ListParty.Add(new ListUnit());
+            ListParty.Add(new ListUnit(new Point (10, 80), 5, 5));
 
             //SW();
             Sound.Music();
