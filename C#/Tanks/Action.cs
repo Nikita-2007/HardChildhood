9c745959f8ea5e65b27a2185ace9d75d19c14b05
Nikita-2007 <nikita2007ua@gmail.com>
2020-06-11 21:20:26 +0300

    Create Action.cs

---
+++ C#/Tanks/Action.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Tanks
+{
+    class Action
+    {
+        public void ActUnit(List<ListUnit> ListParty, ListShot listShot)
+        {
+            foreach (ListUnit party in ListParty)
+                foreach (dynamic unit in party.listUnits)
+                {
+                    unit.PositionUnit();
+                    unit.vector = unit.Vector(unit.vector, unit.speed);
+                }    
+        }
+    }
+}
