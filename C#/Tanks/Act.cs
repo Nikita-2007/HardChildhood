f07a1352ac70d586a20af0e049f1ef33bf01c831
Nikita-2007 <nikita2007ua@gmail.com>
2020-06-12 18:00:20 +0300

    Create Act.cs

---
+++ C#/Tanks/Act.cs
@@ -0,0 +1,11 @@
+﻿namespace Tanks
+{
+    public enum Act
+    {
+        WAIT, //Ожидание
+        FIND, //Поиск
+        MOVE, //Передвижение
+        FIRE, //Стрелять
+        DEAD  //Смерть
+    }
+}
