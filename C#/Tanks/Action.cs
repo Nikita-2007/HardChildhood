32366ea3fc4d1efc982a1be8121b6736a108538b
Nikita-2007 <nikita2007ua@gmail.com>
2020-06-12 18:00:21 +0300

    Update Action.cs

--- C#/Tanks/Action.cs
+++ C#/Tanks/Action.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections.Generic;
-using System.Drawing;
 
 namespace Tanks
 {
     class Action
     {
+        //Перебор всех юнитов
         public void ActUnit(List<ListUnit> ListParty, ListShot listShot)
         {
             foreach (ListUnit party in ListParty)
                 foreach (dynamic unit in party.listUnits)
+                    if (unit.act != Act.DEAD) Logic(unit);
+        }
+
+        //Лагика
+        public void Logic(dynamic unit)
+        {
+            {
+                unit.PositionUnit();
+                unit.vector = unit.Vector(unit.vector, unit.speed);
+
+                switch (unit.act)
                 {
-                    unit.PositionUnit();
-                    unit.vector = unit.Vector(unit.vector, unit.speed);
-                }    
+                    case Act.WAIT:
+                        break;
+
+                    case Act.FIND:
+                        break;
+
+                    case Act.MOVE:
+                        break;
+
+                    case Act.FIRE:
+                        break;
+
+                    default:
+                        unit.Act = Act.WAIT;
+                        break;
+                }
+            }
         }
     }
 }
