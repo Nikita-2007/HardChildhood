09608ef1efc9c52c572dc39884d677defe324ee1
Nikita-2007 <nikita2007ua@gmail.com>
2020-06-25 20:10:28 +0300

    Create Shooting.cs

---
+++ C#/Tanks/Script/Shooting.cs
@@ -0,0 +1,22 @@
+﻿using Game2d;
+using System.Collections.Generic;
+
+namespace Tanks
+{
+    class Shooting
+    {
+        //Расчет стрельбы
+        public void ActShot(List<ListUnit> ListParty, ListShot listShot)
+        {
+            foreach (Shot shot in listShot.listShot)
+            {
+                shot.MoveShot();
+                float findDelta;
+                dynamic findUnit = 1;
+                //findDelta = Func2D.Delta(unit.position, findUnit.position);
+                //if (shot.speed < 2 || shot.position == )
+                //    listShot.RemoveShot(shot);
+            }
+        }
+    }
+}
