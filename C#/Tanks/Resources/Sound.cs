64e1ba5f80b1882aaa15623527f471b3387451a3
Nikita-2007 <nikita2007ua@gmail.com>
2020-06-28 14:02:18 +0300

    Update Sound.cs

--- C#/Tanks/Resources/Sound.cs
+++ C#/Tanks/Resources/Sound.cs
@@ -5,8 +5,20 @@ namespace Tanks
 {
     class Sound
     {
+
+        //Звук выстрела
+        async public static void Shot()
+        {
+            await Task.Run(() => Console.Beep(400, 50));
+        }
+
+        //Звук взрыва
+        async public static void Bang()
+        {
+            await Task.Run(() => Console.Beep(100, 100));
+        }
         //Звук заставки
-       public static void SW()
+        public static void SW()
        {
            Console.Beep(440, 500);
            Console.Beep(440, 500);
