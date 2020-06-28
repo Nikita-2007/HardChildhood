542a3af2245b0fec339be44531b184738738fc58
Nikita-2007 <nikita2007ua@gmail.com>
2020-06-28 14:02:12 +0300

    Create Cratar.cs

---
+++ C#/Tanks/Object/Cratar.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace Tanks
+{
+    class Cratar
+    {
+        public PointF position;
+        public byte time;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Cratar(PointF position)
+        {
+            this.position = position;
+        }
+
+        public void DrawCratar(Graphics g)
+        {
+            g.TranslateTransform(position.X, position.Y);
+            g.FillEllipse(new SolidBrush(Color.FromArgb(200, 0, 0, 0)),
+                new RectangleF(-35, -35, 70, 70));
+            g.ResetTransform();
+        }
+    }
+}
