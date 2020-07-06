b0a2101b6e6fdace60b1ef5d4089209d085f0011
Nikita-2007 <nikita2007ua@gmail.com>
2020-07-06 21:11:39 +0300

    Update Action.cs

--- C#/Tanks/Script/Action.cs
+++ C#/Tanks/Script/Action.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace Tanks
@@ -7,6 +8,7 @@ namespace Tanks
     {
         private List<ListUnit> ListParty;
         private ListShot listShot;
+        private readonly Random random = new Random();
 
         //Перебор всех юнитов
         public void ActUnit(List<ListUnit> ListParty, ListShot listShot)
@@ -16,7 +18,8 @@ namespace Tanks
 
             foreach (ListUnit party in ListParty)
                 foreach (dynamic unit in party.listUnits)
-                    if (unit.act != Act.DEAD) Logic(unit);
+                    if (unit.act != Act.DEAD)
+                        Logic(unit);
         }
 
         //Логика
@@ -53,25 +56,57 @@ namespace Tanks
         {
             //Если танк мёртв
             if (unit.life <= 0)
-                unit.act = Act.DEAD;
+                KillUnit(unit);
 
             //Если надо найти танк
             else
-                ActFIND(unit);
-            unit.act = Act.FIRE;
-        }
+            {
+                //Поиск ближайщего танка
+                float findDelta = unit.vision, minDelta = unit.vision;
+                foreach (ListUnit party in ListParty)
+                    foreach (dynamic findUnit in party.listUnits)
+                    {
+                        if (findUnit.act != Act.DEAD && findUnit.color != unit.color)
+                            findDelta = unit.Delta(unit.position, findUnit.position);
+                        if (findDelta < minDelta)
+                        {
+                            minDelta = findDelta;
+                            unit.target = findUnit.position;
+                        }
+                    }
 
+                //Прверка на атаку
+                if (minDelta < 512)
+                {
+                    unit.act = Act.FIRE;
+                }
+
+                //Проверка на движение
+                else if (minDelta < 1024)
+                {
+                    unit.act = Act.MOVE;
+                }
+
+                //Поиск цели
+                else
+                {
+                    unit.target.X += unit.position.X + random.Next(-128, 128);
+                    unit.target.Y += unit.position.Y + random.Next(-128, 128);
+                    unit.act = Act.FIND;
+                }
+            }
+        }
         //Процес поиска
         private void ActFIND(dynamic unit)
         {
-            //Юнит едит пока не найдёт цель, башня прямо, через 1 секунду - WAIT
-            FindTarget(unit);
+            //Юнит едит пока не найдёт цель, башня прямо
         }
 
         //Процес сближения
         private void ActMOVE(dynamic unit)
         {
             //Юнит едит, башня на цель, когда сможет стрелять - FIRE
+            
         }
 
         //Процес атаки
@@ -85,24 +120,17 @@ namespace Tanks
             {
                 listShot.NewShot(unit);
                 unit.timeShot = 0;
+                unit.act = Act.WAIT;
             }
         }
 
-        //Поиск цели
-        private PointF FindTarget(dynamic unit)
-        {            
-            float findDelta, minDelta = 500;
-            foreach (ListUnit party in ListParty)
-                foreach (dynamic findUnit in party.listUnits)
-                {
-                    findDelta = unit.Delta(unit.position, findUnit.position);
-                    if (findDelta < minDelta)
-                    {
-                        minDelta = findDelta;
-                        unit.target = findUnit.position;
-                    }
-                }
-            return unit.target;
+        //Смерть юнита
+        private void KillUnit(dynamic unit)
+        {
+            unit.act = Act.DEAD;
+            unit.speed = 0.0f;
+            unit.life = 0.0f;
+            unit.color = Color.Black;
         }
     }
 }
