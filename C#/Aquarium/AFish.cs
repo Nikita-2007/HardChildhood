79dadb24f6bc0f7c9efcc450f2afd810d56aa730
Nikita-2007 <nikita2007ua@gmail.com>
2020-07-13 21:24:51 +0300

    Create AFish.cs

---
+++ C#/Aquarium/AFish.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+
+namespace Aquarium
+{
+    abstract class AFish
+    {
+        public Random random = new Random();
+
+        public PointF target = new PointF();
+
+    }
+}
