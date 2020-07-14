3b8930578caf6656bcd992615d6139f399f7ec9d
Nikita-2007 <nikita2007ua@gmail.com>
2020-07-14 16:07:53 +0300

    Update Fishes.cs

--- C#/Aquarium/Fishes.cs
+++ C#/Aquarium/Fishes.cs
@@ -1,10 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace Aquarium
 {
     class Fishes
     {
         public List<object> listfish = new List<object>();
+        Random random = new Random();
 
+        public Fishes()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                NewFish(new Fish1());
+                NewFish(new Fish2());
+                NewFish(new Fish3());
+            }
+        }
+
+        private void NewFish(dynamic fish)
+        {
+            listfish.Add(fish);
+            fish.position = Position();
+            fish.target = Position();
+
+        }
+        public PointF Position()
+        {
+            PointF position = new PointF();
+            position.X = random.Next(0, 1800);
+            position.Y = random.Next(0, 1000);
+
+            return position;
+        }
+
+        public void DrawListFish(Graphics g)
+        {
+            foreach (dynamic fish in listfish)
+            {
+                fish.Target();
+                fish.Position();                
+                fish.DrawFish(g);              
+            }
+        }
     }
 }
