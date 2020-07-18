87a07910a7d1f5c1fcba585de41afce9dfc82fb5
Nikita-2007 <nikita2007ua@gmail.com>
2020-07-18 21:19:34 +0300

    Create Babble.cs

---
+++ C#/Aquarium/Babble.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace Aquarium
+{
+    class Babble : AFish
+    {
+        public Bitmap bitmap = Properties.Resources.buble;
+
+        //Отрисовка Рыбы
+        public void DrawFish(Graphics g)
+        {
+            PositionBubble(bitmap);
+
+            g.DrawImage(bitmap, position.X, position.Y);
+        }
+    }
+}
