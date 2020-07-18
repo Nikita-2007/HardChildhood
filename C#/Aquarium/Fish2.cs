c6737c19e1f776a460b43a8eb067d04dee889d47
Nikita-2007 <nikita2007ua@gmail.com>
2020-07-18 21:19:44 +0300

    Update Fish2.cs

--- C#/Aquarium/Fish2.cs
+++ C#/Aquarium/Fish2.cs
@@ -4,13 +4,14 @@ namespace Aquarium
 {
     class Fish2 : AFish, IFish
     {
-        private Bitmap bitmap = Properties.Resources._2fish;
+        public Bitmap bitmap = Properties.Resources._2fish;
 
         //Отрисовка Рыбы
         public void DrawFish(Graphics g)
         {
             Target();
             Position(bitmap);
+            fish123 = 2;
 
             g.DrawImage(bitmap, position.X, position.Y);
         }
