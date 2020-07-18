2b8d60b4f6d3b1ef9615e00784e77083b875ec14
Nikita-2007 <nikita2007ua@gmail.com>
2020-07-18 15:36:06 +0300

    Update AFish.cs

--- C#/Aquarium/AFish.cs
+++ C#/Aquarium/AFish.cs
@@ -10,22 +10,30 @@ namespace Aquarium
 
         private Random random = new Random();       
         private byte x = 1;
+        private byte time1 = 0;
+        private int time2 = 0;
 
         //Расчет Позиции
         public PointF Position(Bitmap bitmap)
-        {            
+        {
+            if (time1 == 60)
+                time1 = 0;
+
+            if (time2 >= 180)
+                time2 = 0;
+
             if (target.X > position.X)
             {
-                if (x == 1)
+                if (x == 1 && time1 == 0)
                 {
                     bitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
                     x = 2;
                 }
-                position.X ++;
+                position.X++;
             }
             else
             {
-                if (x == 2)
+                if (x == 2 && time1 == 0)
                 {
                     bitmap.RotateFlip(RotateFlipType.Rotate180FlipY);
                     x = 1;
@@ -35,9 +43,12 @@ namespace Aquarium
 
             if (target.Y > position.Y)
                 position.Y++;
+                
             else
                 position.Y--;
-
+            time1++;
+            time2 += random.Next(-10, 10);
+            time2++;
 
             return position;
         }
@@ -45,12 +56,13 @@ namespace Aquarium
         //Расчeт цели
         public PointF Target()
         {
-            if (position.X == target.X || position.Y == target.Y)
+            if (position.X == target.X && position.Y == target.Y || time2 >= 160)
             {
                 target.X = random.Next(0, 1800);
                 target.Y = random.Next(0, 1080);
             }
-            return target;
+
+            return target;           
         }
     }
 }
