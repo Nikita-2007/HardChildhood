fa993204a308e4924c07e6e922c501f04560587f
Nikita-2007 <nikita2007ua@gmail.com>
2020-08-07 18:21:05 +0300

    Update Task1.py

--- Python/Task1.py
+++ Python/Task1.py
@@ -1,16 +1,19 @@
-import os
 # Task 1
+import os 
+
+os.system('mode con cols=40 lines=10')
+
 while 0 < 1:  
     x = input("Введите год - ")
     y = int(x)%4
     z = int(x)%100
     a = int(x)%400
 
-    if y == 0 and z != 0:
-        print("Высокосный")
-    elif a == 0:
+    if y == 0 and z != 0 or a == 0:
         print("Высокосный")
     else:
         print("Не Высокосный")
-    input()
-    os.system('cls')
+    input('Нажмите Enter для продолжения...')
+    os.system('cls||clear')
+    
+#if 4  x%4 == 0 and  x%100 or != 0 400:
