19d8af9ef515441a8b86b884037a62e4c0da167e
Nikita-2007 <nikita2007ua@gmail.com>
2020-08-10 20:10:16 +0300

    Create Lesson2 - Данные.py

---
+++ Python/Lessons/Lesson2 - Данные.py
@@ -0,0 +1,68 @@
+# Yrock 2
+
+print(1+2+3+4+5)
+print("1+2+3+4+5")
+print("")
+
+x = True
+print(x)
+print(type(x))
+print("")
+
+x = 5
+print(x)
+print(type(x))
+print("")
+
+x = 5.0
+print(x)
+print(type(x))
+print("")
+
+x = "5.0"
+print(x)
+print(type(x))
+print("")
+
+
+x = 33.9e3
+print(x)
+print("")
+
+x = 33.9e-3
+print(x)
+print("")
+
+print(7/2)
+print(7//2)
+print(7%2)
+print("")
+
+print(5**3)
+print(25**0.5)
+print("")
+
+x = 10
+x += 5
+print(x)
+print("")
+
+x = "2"
+y = 3
+z = int(x)+y
+print(z)
+print("")
+
+x2 = 0b101
+x8 = 0o11
+x16 = 0x0a
+print(x2, x8, x16)
+print("")
+
+x,y,z = 0
+x = y = z = 0
+x,y = y,x
+print(x, y, z)
+print("")
+
+input()
