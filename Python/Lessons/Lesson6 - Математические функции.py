9babfad2f58bb17afd2410b3ba9690fac137515e
Nikita-2007 <nikita2007ua@gmail.com>
2020-08-14 18:23:25 +0300

    Create Lesson6 - Математические функции.py

---
+++ Python/Lessons/Lesson6 - Математические функции.py
@@ -0,0 +1,28 @@
+# Yrock 6
+import math
+x=5
+
+print("Число p", math.pi)
+print("Число експонентно", math.e)
+print("Возведение в степень", math.pow(2,3)) # 2**3
+print("Остаток от деление", math.fmod(7,3)) # 7%3
+print("Проверка на число", math.isfinite(x))
+print("Проверка на бесконечность", math.isinf(x))
+print("мантисса и експонентно", math.frexp(123.456))
+print("Квадратный корень", math.sqrt(9))
+print("Модуль числа", math.fabs(-20))
+print("Модуль числа 1 со знаком числа 2", math.copysign(-10, 5))
+print("Отделяет целую и дробную часть", math.modf(123.456))
+print("Округление в большую сторону", math.ceil(123.456))
+print("Округление в меньшую сторону", math.floor(123.456))
+print("Округление", round(123.416))
+print("Косинус угла", math.cos(math.radians(60)))
+print("Синус угла", math.sin(math.radians(60)))
+print("Тангенст угла", math.tan(math.radians(60)))
+print("Гипотенуза", math.hypot(60))
+print("Логарифм", math.log(8, 2))
+print("Логарифм 10-ый", math.log10(100))
+print("Длина строки", len("123"))
+print("")
+
+input()
