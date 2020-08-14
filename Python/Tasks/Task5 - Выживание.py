3a6180a5b7754bdf7d580de48632b6c8cfa4ef1b
Nikita-2007 <nikita2007ua@gmail.com>
2020-08-14 18:23:09 +0300

    Create Task5 - Выживание.py

---
+++ Python/Tasks/Task5 - Выживание.py
@@ -0,0 +1,15 @@
+﻿# Task 5
+import math
+
+Xv = int(input("Введите свои кординаты по X: "))
+Yv = int(input("Введите свои кординаты по Y: "))
+
+Xb = int(input("Введите взрыв кординаты по X: "))
+Yb = int(input("Введите взрыв кординаты по Y: "))
+x = Xb-Xv
+y = Yb-Yv
+if math.hypot(x, y) > 5000:
+    print("Вы выживете, так как центер взрыва от вас в " + str(math.hypot(x, y)), "метров")
+else:
+    print("Вы умрёте, так как центер взрыва от вас в " + str(math.hypot(x, y)), "метров")
+input()
