c223cc24bc48955c4daeedeeee9ef4023f5f4f21
Nikita-2007 <nikita2007ua@gmail.com>
2020-09-12 21:18:26 +0300

    Update Olom03.py

--- Python/Олемпиада/Olom03.py
+++ Python/Олемпиада/Olom03.py
@@ -1,14 +1,38 @@
-#m = input()
-x = int(input())
-kur = []
-g = 0
-while g < x:
-    l = input()
-    kur.append(l)
-    g += 1
-for i in kur:
-    if kur.count(i) != 1:
-        print(i, kur.count(i))
-        while 
-        kur.remove(i)
+#Ввод
+N = input("Деапозон - ")
+M = int(input("Количество цифр - "))
+arr = input("Список - ")
+
+#Решение
+maxN = 0
+sumN = 0
+
+#Вывод
+print(N, M, arr)
 input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#g = 0
+#while g < M:
+#    l = input()
+#    arr.append(l)
+#    g += 1
+#for i in arr:
+#    if arr.count(i) != 1:
+#        print(i, arr.count(i))
+#        while 
+#        arr.remove(i)
