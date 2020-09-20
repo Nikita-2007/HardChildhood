336a31d1be4e3df2b509ff53c63878c9ba5c7f3e
Nikita-2007 <nikita2007ua@mail.ru>
2020-09-20 21:39:59 +0300

    Create Replay.pas

---
+++ Pascal/Replay.pas
@@ -0,0 +1,16 @@
+﻿program Yrock_Replay;
+
+var x:integer;
+
+begin
+  write('Введите число ');
+  read(x);
+  if (x < 100) and (x > 0)
+  then writeln('x < 100')
+  else writeln('x > 100');
+  while x > 0 do
+  begin
+    x := x - 1;
+    writeln (x);
+  end;
+end.
