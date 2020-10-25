61bd467a83e2c510625a07d64ec40e64d7a4dda9
Nikita-2007 <55892093+Nikita-2007@users.noreply.github.com>
2020-10-25 21:15:32 +0300

    Update 6-2.pas

--- Pascal/EjudgeOlimp2020/6-2.pas
+++ Pascal/EjudgeOlimp2020/6-2.pas
@@ -1,41 +1,39 @@
 ﻿program prog02;
+
 var
-  x, l, temp:integer;
+  x, l, i, j, temp: integer;
   arr: array of integer;
 
 begin
-read(x);
-temp := x;
-while true do
-begin
-x := x div 10;
-if x >= 0 then l := l+1
-else
-  break;
-end;
-setLength(arr, l);
-
-for var i := 0 to l-1 do
-begin
-  arr[i] := x mod 10;
-  x := x div 10;
-end;
-
-for var i := 0 to l-1 do
-  for var j := i to l-1 do
+  read(x);
+  temp := x;
+  while true do
   begin
-  if arr[i] < arr[j] then
-    begin
-      temp := arr[i];
-      arr[i] := arr[j];
-      arr[j] := temp;
-    end;
+    x := x div 10;
+    if x > 0 then l := l + 1
+    else
+      break;
   end;
-x := 0;
-for var i := 0 to l-1 do
-begin
-  x := x*10 + arr[i];
-end;
-
-write(x);
+  l := l + 1;
+  x := temp;
+  setLength(arr, l);
+  for i := 0 to l - 1 do
+  begin
+    arr[i] := x mod 10;
+    x := x div 10;
+  end;
+  for i := 0 to l - 1 do
+    for j := i+1 to l - 1 do
+    begin
+      if arr[i] < arr[j] then
+      begin
+        temp := arr[i];
+        arr[i] := arr[j];
+        arr[j] := temp;
+      end;
+    end;
+  x := 0;
+  for i := 0 to l - 1 do
+    x := x * 10 + arr[i];
+  write(x);
 end.
