9a04e0fd3b170a5106d9dd42a104387116fd117d
Nikita-2007 <55892093+Nikita-2007@users.noreply.github.com>
2020-10-25 20:35:19 +0300

    Create Program1.pas

---
+++ Pascal/Lesion/Program1.pas
@@ -0,0 +1,19 @@
+﻿program hello;
+uses crt; 
+var x,y,z:integer;
+
+begin
+  
+writeln ('Ведите число');
+readln (x);
+ClrScr;
+writeln ('Ведите другое число');
+read (y);
+ClrScr;
+writeln ('Сложите эти числа');
+read (z);
+ClrScr;
+writeln (x+y);
+writeln ('НЕОЖИДАЛОЧКА');
+
+end.
