9713233d505a02bb2e4f184f00463f96e3ae3a03
Nikita-2007 <55892093+Nikita-2007@users.noreply.github.com>
2020-10-25 20:35:17 +0300

    Create miror.pas

---
+++ Pascal/Lesion/miror.pas
@@ -0,0 +1,28 @@
+﻿program miror;
+
+const
+    M = 10;
+    
+var r:integer;
+  arr: array [1..M] of integer;
+  
+  
+ 
+begin
+  
+for var i := 1 to M do arr[i] := i * 2;
+for var i := 1 to M do write(arr[i], ' ');
+writeln;
+writeln;
+
+//3EPKALO
+  for var i := 1 to M div 2 do
+  begin
+  r := arr[i];
+  arr[i] := arr[M -i+1];
+  arr[M -i+1] := r;
+  end;
+   
+  for var i := 1 to M do write(arr[i], (' '));
+  
+end.
