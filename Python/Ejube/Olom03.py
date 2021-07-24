70bd7ec991eacf185784d12b5f228698ce385741
unknown <55892093+Nikita-2007@users.noreply.github.com>
2021-07-24 14:53:16 +0300

    4

--- Python/Ejube/Olom03.py
+++ Python/Ejube/Olom03.py
@@ -1,13 +1,23 @@
-N, H = map(int, input().split())
+N, M = map(int, input().split())
 arr = list(map(int, input().split()))
 
-x = 0
-for i in range(N):
-    if x < arr.count(arr[i]):
-        x = arr.count(arr[i])
-if x < H:
-    leng = H - x
-elif x >= H:
-    leng = 0
+arr.sort()
+lest = 0
+max_counter = 1
+start_counter = 0
 
-print(leng)
+for i in arr:
+    if i > lest:
+        counter = 1
+        lest = i
+        start_counter = arr.index(i)
+    else:
+        counter += 1
+    if counter >= M:
+        max_counter = M
+        break
+    if counter >= max_counter:
+        if start_counter >= M-counter:
+            max_counter = counter
+
+print(M - max_counter)
--- Python/Ejube/Olom04.py
+++ Python/Ejube/Olom04.py
@@ -1,14 +1,6 @@
-x = int(input())
-c = 0
-kur = []
+N = int(input())
 
-while c < x:
-    m = input()
-    kur.append(m)
-    c += 1
-for i in kur:
-    k = int(i)
-    if k <= 100 or (k-100)%7 != 0:
-        print(-1)
-    else:
-        print((k-100)//7)
+for i in range(N):
+    arr = list(map(int, input().split()))
+    
+print(0)
