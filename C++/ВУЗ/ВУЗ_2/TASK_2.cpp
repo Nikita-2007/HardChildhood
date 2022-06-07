e236990eaea8dc4d1c3f5f72d2072f5ba26ac988
Nikita-2007 <nikita2007ua@gmail.com>
2022-06-07 21:19:36 +0300

    ВУЗ 4

--- C++/ВУЗ/ВУЗ_2/TASK_2.cpp
+++ C++/ВУЗ/ВУЗ_2/TASK_2.cpp
@@ -6,10 +6,10 @@ int main() {
 	setlocale(LC_ALL, "RUS");
 	int A1 = 3;
 	int B1 = 3.14;
-	cout << A1 << endl << B1 << endl;
+	cout << "int A = 3, A = " << A1 << endl << "int B = 3.14, B = " << B1 << endl;
 	double A2 = 3;
 	double B2 = 3.14;
-	cout << A2 << endl << B2 << endl;
+	cout << "double A = 3, A = " << A2 << endl << "double B = 3.14, B = " << B2 << endl;
 
 	system("pause>nul");
 	return 0;
