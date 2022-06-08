0edff203136391b5e2fb437a0f7196b56ed4a7b7
Nikita-2007 <nikita2007ua@gmail.com>
2022-06-08 19:42:50 +0300

    Бинырные условия

---
+++ C++/Бинарыне опперации/Бинарыне опперации.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    cout << "Бинорное значение" << endl;
+    int x = 0b0011;
+    int y = 0x5;
+    int z;
+    cout << "x равно: " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y равно: " << y << ", бинарно: " << bitset<8>(y) << endl;
+
+    cout << endl << "Бинорное НЕ" << endl;
+    x = ~x;
+    y = ~y;
+    cout << "x равно: " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y равно: " << y << ", бинарно: " << bitset<8>(y) << endl;
+
+    cout << endl << "Бинорное И" << endl;
+    x = 3, y = 5;
+    z = x & y;
+    cout << "x равно: " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y равно: " << y << ", бинарно: " << bitset<8>(y) << endl;
+    cout << "z равно: " << z << ", бинарно: " << bitset<8>(z) << endl;
+
+    cout << endl << "Бинорное ИЛИ" << endl;
+    z = x | y;
+    cout << "x равно: " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y равно: " << y << ", бинарно: " << bitset<8>(y) << endl;
+    cout << "z равно: " << z << ", бинарно: " << bitset<8>(z) << endl;
+
+    cout << endl << "Бинорное исключающие ИЛИ(ЛИБО)" << endl;
+    z = x ^ y;
+    cout << "x равно: " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y равно: " << y << ", бинарно: " << bitset<8>(y) << endl;
+    cout << "z равно: " << z << ", бинарно: " << bitset<8>(z) << endl;
+
+    cout << endl << "Побитовый сдвиг на шаг в левво" << endl;
+    y = x >> 1;
+    cout << "x равно: " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y равно: " << y << ", бинарно: " << bitset<8>(y) << endl;
+
+    cout << endl << "Побитовый сдвиг на шаг в право" << endl;
+    y = x << 1;
+    cout << "x равно: " << x << ", бинарно: " << bitset<8>(x) << endl;
+    cout << "y равно: " << y << ", бинарно: " << bitset<8>(y) << endl;
+
+    cout << endl << (x &= 5) << endl;
+
+    system("pause>nul");
+    return 0;
+}
