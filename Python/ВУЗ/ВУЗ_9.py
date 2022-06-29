ecfd92c8fa90c4883882f08d63d87fe322b2fd79
Nikita-2007 <nikita2007ua@gmail.com>
2022-06-29 21:34:31 +0300

    Вуз 9 питон

--- Python/ВУЗ/ВУЗ_9.py
+++ Python/ВУЗ/ВУЗ_9.py
@@ -2,11 +2,11 @@ print("Введите время прихода первого человека:
 h1, m1 = map(int, input().split(':'))
 print("Введите время прихода второго человека:")
 h2, m2 = map(int, input().split(':'))
-mini = min((h1 * 60 + m1), (h2 * 60 + m2))
-maxi = max((h1 * 60 + m1), (h2 * 60 + m2))
-if (mini < 15):
-    mini += 1440
-if (maxi - mini <= 15):
-    print("Встреча состоится")
+if (h1 < 0 or h1 > 23 or m1 < 0 or m1 > 59 or h2 < 0 or h2 > 23 or m2 < 0 or m2 > 59):
+    print("Некоректные данные")
 else:
-    print("Встреча не состоится")
+    d = abs((h1*60+m1) - (h2*60+m2))
+    if (d < 15 or d > 1425):
+        print("Встреча состоится")
+    else:
+        print("Встреча не состоится")
