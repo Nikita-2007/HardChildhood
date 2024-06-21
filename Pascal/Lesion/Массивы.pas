0655fa2737ae27a259bd45c08fb78087dc48259d
Nikita <55892093+Nikita-2007@users.noreply.github.com>
2024-06-21 14:53:18 +0300

    Create Массивы.pas

---
+++ Pascal/Lesion/Массивы.pas
@@ -0,0 +1,15 @@
+﻿program massivi;
+var
+  statArr: array[0..5] of integer;
+  dynArr: array of integer;
+  x: byte;
+
+begin
+  statArr[0] := 111;
+  writeln(statArr[0]);
+  
+  read(x);
+  setlength(dynArr, x);
+  dynArr[0] := 222;
+  writeln(dynArr);
+end.
