40c7ac1adcef26e44d527c0a13b52271ca5a1897
Nikita <55892093+Nikita-2007@users.noreply.github.com>
2024-06-21 14:55:12 +0300

    Create Волны.cpp

---
+++ C++/Задачи C++/1 - 13 Вывод чисел волнами/Волны.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main() {
+    int n, i = 1, w = 3, s = 1, k = 1;
+    std::cin >> n;
+    system("cls");
+    while (i <= n) {
+        for (int j = 0; j < k && i <= n; ++j)
+            std::cout << i++ << " ";
+        std::cout << std::endl;
+        k += s;
+        if (k < 1) {
+            s = 1;
+            k = 2;
+            ++w;
+        }
+        else if (k >= w) {
+            s = -1;
+            k -= 2;
+        }
+    }
+    return 0;
+}
