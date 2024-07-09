615ad5fcd90b3415336eee94a564b1c02520520c
Nikita <55892093+Nikita-2007@users.noreply.github.com>
2024-07-09 20:44:52 +0300

    4-2

---
+++ C++/Задачи C++/4 - 2 - Количество выполненных задач/2. Количество выполненных задач.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <fstream>
+#include "json.hpp"
+
+int main() {
+    std::ifstream i("data.json");
+    nlohmann::json j;
+    i >> j;
+
+    int user_id;
+    std::cin >> user_id;
+
+    int completed_tasks = 0;
+    for (const auto& project : j) {
+        for (const auto& task : project["tasks"]) {
+            if (task["user_id"] == user_id && task["completed"] == true) {
+                completed_tasks++;
+            }
+        }
+    }
+
+    std::cout << completed_tasks << std::endl;
+
+    return 0;
+}
