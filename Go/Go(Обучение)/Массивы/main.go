406f2cbd3435ac4a8c410fbb3eb3c2a70c1e54c3
Nikita <55892093+Nikita-2007@users.noreply.github.com>
2024-07-20 11:26:45 +0300

    Двумерные массивы

--- Go/Go(Обучение)/Массивы/main.go
+++ Go/Go(Обучение)/Массивы/main.go
@@ -21,4 +21,8 @@ func main() {
 	}
 	var res float64 = sum / float64(len(marks))
 	fmt.Println(res)
+
+	arr2d := [3][4]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}
+	fmt.Println(arr2d)
+	fmt.Println(arr2d[1][2])
 }
