647f5f0cae857a48008cf71acd880943cf45e095
Nikita <nikita2007ua@gmail.com>
2024-08-01 18:32:25 +0300

    Возложение основания

--- Python/Notebook/app.py
+++ Python/Notebook/app.py
@@ -5,11 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    data = {
+        'title':'Главная страница',
+        'content':'Тест индекс'
+    }
+    return render_template("index.html", data=data)
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    data = {
+        'title':'О проекте',
+        'content':'Тест описания'
+    }
+    return render_template("about.html", data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
