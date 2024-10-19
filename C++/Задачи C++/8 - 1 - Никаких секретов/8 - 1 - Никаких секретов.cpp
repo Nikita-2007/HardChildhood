a7677ff0f53b8813a0ef697d33831a6dba5ced69
Nikita <nikita2007ua@gmail.com>
2024-10-20 00:49:32 +0300

    8-1

---
+++ C++/Задачи C++/8 - 1 - Никаких секретов/8 - 1 - Никаких секретов.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+class Keeper {
+    int trash;
+    int secret;
+
+public:
+    Keeper() {
+        std::cin >> secret;
+    }
+};
+
+// Ваш код здесь //////////////////////
+class Hack {
+public:
+    int hack_trash, hack_secret;
+};
+
+int& hack_it(Keeper &keeper) {
+    return ((Hack*)(&keeper))->hack_secret;
+};
+///////////////////////////////////////
+
+int main() {
+    Keeper keeper;
+    int secret = hack_it(keeper);
+    std::cout << secret;
+}
